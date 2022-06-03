0692d1fa5ab8cd3078c17c78e69d066d8cf2cd02
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2022-06-03 17:40:48 +0300

    Add attach accessory page

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -3,11 +3,15 @@ const router = require('express').Router();
 const accessoryService = require('../services/accessoryService');
 router.get('/create', function (req, res){
     res.render('accessory/create');
-})
+});
 
 router.post('/create', function (req, res){
     accessoryService.createAccessory(req.body);
     res.redirect('/');
-})
+});
+
+router.get('/:id/attach', function (req, res){
+    res.render('accessory/attach');
+});
 
 module.exports = router;

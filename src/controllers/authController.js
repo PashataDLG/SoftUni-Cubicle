e1b1473727cdb70d525f72a4d0bfe3a652c0d516
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2022-06-09 13:54:14 +0300

    Add user model

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,23 @@
 const router = require('express').Router();
+const authService = require('../services/authService');
 
 router.get('/login', function (req, res) {
     res.render('login');
 });
 
+router.post('/login', function (req, res) {
+    console.log(req.body);
+     
+    res.redirect('/auth/login');
+})
+
 router.get('/register', function (req, res) {
     res.render('register');
 });
 
+router.post('/register', function (req, res) {
+    let { username, password } = req.body;
+    authService.register(username, password, repeatPassword);
+});
 
 module.exports = router;
---
+++ src/services/authService.js
@@ -0,0 +1,5 @@
+const bcrypt = require('bcrypt');
+
+exports.register = (username, password, repeatPassword) => {
+    console.log(username, password, repeatPassword);
+}
